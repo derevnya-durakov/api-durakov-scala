bb3e525b88885dd041932a1f6bf2d86229f06646
ntop <toporkov.kolya96@gmail.com>
2021-01-05 22:40:54 +0300

    use require to validate input parameters

--- src/main/scala/durak/cards/CardDeck.scala
+++ src/main/scala/durak/cards/CardDeck.scala
@@ -23,23 +23,20 @@ class CardDeck private(private var cards: List[Card]) {
 }
 
 object CardDeck {
-  private val maxDeckSize = 52;
+  private val MAX_DECK_SIZE = 52;
+  private val ACE_VALUE = 14;
 
   def create36Dec: CardDeck = CardDeck(36)
 
   def create52Deck: CardDeck = CardDeck(52)
 
   def apply(deckSize: Int): CardDeck = {
-    if (deckSize > maxDeckSize)
-      throw new IllegalArgumentException(s"Max deck size is $maxDeckSize")
-    if (deckSize <= 0)
-      throw new IllegalArgumentException("Deck size should be positive")
+    require(deckSize <= MAX_DECK_SIZE, s"Max deck size is $MAX_DECK_SIZE")
+    require(deckSize > 0, "Deck size should be positive")
     val suitsCount = CardSuit.suits.length;
-    if (deckSize % suitsCount != 0)
-      throw new IllegalArgumentException(s"Cards amount should be divisible by $suitsCount")
+    require(deckSize % suitsCount == 0, s"Cards amount should be divisible by $suitsCount")
     val cardsInSuit = deckSize / suitsCount
-    val aceValue = 14;
-    val values = (aceValue until (aceValue - cardsInSuit) by -1).map(value => new CardValue(value))
+    val values = (ACE_VALUE until (ACE_VALUE - cardsInSuit) by -1).map(value => new CardValue(value))
     val cards = CardSuit.suits.flatMap(suit => values.map(value => new Card(suit, value)))
     new CardDeck(cards)
   }
