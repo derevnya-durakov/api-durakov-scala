ab3408af4cd3c370edd3541e69339c8db023a22f
ntop <toporkov.kolya96@gmail.com>
2021-01-05 20:57:00 +0300

    use App trait instead explicit main method

--- src/main/scala/durak/Main.scala
+++ src/main/scala/durak/Main.scala
@@ -2,10 +2,8 @@ package durak
 
 import durak.cards.CardDeck
 
-object Main {
-  def main(args: Array[String]): Unit = {
-    val deck: CardDeck = CardDeck.create52Deck.shuffled
-    while (deck.nonEmpty)
-      println(deck.pop().get)
-  }
+object Main extends App {
+  val deck: CardDeck = CardDeck.create52Deck.shuffled
+  while (deck.nonEmpty)
+    println(deck.pop().get)
 }
